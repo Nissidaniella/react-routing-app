57daa0cf5ccfaf010f863f3882fdf990509da028
Nissi Daniella Diallo <nissidaniella@yahoo.com>
2024-02-13 01:43:53 +0000

    fifth commit

---
+++ src/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Services() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>We offer a range of services:</p>
+      <ul>
+        <li><NavLink to="/services/1">Service 1</NavLink></li>
+        <li><NavLink to="/services/2">Service 2</NavLink></li>
+        <li><NavLink to="/services/3">Service 3</NavLink></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;
